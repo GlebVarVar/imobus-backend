6c1af8e462e82e854fb27adfc05161bb81e61ce3
Gleb Kostrov <anegada1@mail.ru>
2022-07-18 12:38:42 +0300

    fix cors

--- index.js
+++ index.js
@@ -3,12 +3,9 @@ const cors = require('cors');
 
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
-const corsOptions = {
-    origin: "https://imobus-front.vercel.app/",
-    }
-app.use(cors(corsOptions));
+
 
 
 app.use(express.static('public'));
